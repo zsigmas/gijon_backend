d20118850af12aae4819beb2ad4d96b023471102
zsigmas <luis.moris.fernandez+gitkraken@gmail.com>
2020-07-05 18:00:26 +0200

    Added map endpoint; returns geojson Added test endpoint; check variable passed by url

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
+import logging
+import json
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(levelname)s:%(funcName)s:[%(asctime)s]: %(message)s",
+                    datefmt="%H:%M:%S")
+log = logging.getLogger(__name__)
 
 app = Flask(__name__)
+api = Api(app)
 
+
+# Flask Definition
 
 @app.route('/')
 def hello_world():
+    log.info('Serving')
     return 'Hello World!'
 
+# API definition
+class ShowMap(Resource):
+    def get(self):
+        with open('../external_data/barrios-urbanos.geojson') as f:
+            data = json.load(f)
+            return data
+
+class test(Resource):
+    def get(self, num):
+        return num
+
+api.add_resource(ShowMap, '/map')
+api.add_resource(test, '/test/<int:num>')
 
 if __name__ == '__main__':
     app.run()
